0e47f7572ef7c919966cae999ee053ca437e8480
Joseph Wakeling <josephjwakeling@gmail.com>
2022-06-25 15:11:35 +0100

    write remaining label tests

--- test/pattern_tests/LabelTest.java
+++ test/pattern_tests/LabelTest.java
@@ -72,6 +72,129 @@ public class LabelTest {
 		);
 	}
 	
+	@Test
+	public void testc1_1() {
+		testUnsafePattern(
+			new boolean[][] {
+				{true, false, true},
+				{false, false, false}
+				{false, false, false}
+			},
+			"c1,1"
+		);
+	}
+	
+	@Test
+	public void testc1_01() {
+		testUnsafePattern(
+			new boolean[][] {
+				{true, false, false},
+				{false, false, true},
+				{false, false, false}
+			},
+			"c1,01"
+		);
+	}
+	
+	@Test
+	public void testc1_0_1() {
+		testUnsafePattern(
+			new boolean[][] {
+				{true, false, false},
+				{false, false, false},
+				{false, false, true}
+			},
+			"c1,0,1"
+		);
+	}
+	
+	@Test
+	public void testc2_1() {
+		testUnsafePattern(
+			new boolean[][] {
+				{true, true, true},
+				{false, false, false},
+				{false, false, false}
+			},
+			"c2,1"
+		);
+	}
+	
+	@Test
+	public void c01_01_01() {
+		testUnsafePattern(
+			new boolean[][] {
+				{false, true, false},
+				{false, false, true},
+				{false, true, false}
+			},
+			"c01,01,01"
+		);
+	}
+	
+	@Test
+	public void testc2_01_0_1() {
+		testUnsafePattern(
+			new boolean[][] {
+				{true, true, false},
+				{false, false, true},
+				{true, false, false}
+			},
+			"c2,01,0,1"
+		);
+	}
+	
+	@Test
+	public void testc2_0_1_1a1() {
+		testUnsafePattern(
+			new boolean[][] {
+				{true, true, false},
+				{false, true, false},
+				{true, false, true}
+			},
+			"c2,0,1,1a1"
+		);
+	}
+	
+	@Test
+	public void testc2_2_2_2a1() {
+		testUnsafePattern(
+			new boolean[][] {
+				{true, true, true},
+				{true, true, true},
+				{true, true, true}
+			},
+			"c2,2,2,2a1"
+		);
+	}
+	
+	@Test
+	public void testd02_02_01_001b0_0_0_1() {
+		testUnsafePattern(
+			new boolean[][] {
+				{false, true, true, false},
+				{true, false, false, true},
+				{false, true, false, true},
+				{false, false, true, false}
+			},
+			"d02,02,01,001b0,0,0,1"
+		);
+	}
+	
+	@Test
+	public void teste0101_01_0_0001c1_2_0_01() {
+		testUnsafePattern(
+			new boolean[][] {
+				{false, true, false, true, false},
+				{true, true, false, true, true},
+				{false, true, false, true, false},
+				{false, false, false, false, false},
+				{false, false, false, false, false}
+			},
+			"e0101,01,0,0001c1,2,0,01"
+		);
+	}
+	
 	private void testUnsafePattern(boolean[][] cells, String expected) {
 		test(new UnsafePattern(cells), expected);
 	}
